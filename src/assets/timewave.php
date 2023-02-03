89bf61aac25bced29ed79e2f8cefe28647a83d08
Andre Hansson <andre.hansson@etimo.se>
2023-02-03 09:14:55 +0100

    new version number

--- src/assets/timewave.php
+++ src/assets/timewave.php
@@ -1,7 +1,8 @@
 <?php
+
 return array(
     // Version (is also used to prevent using of cached js files on update)
-    'version' => '2.6.4',
+    'version' => '2.6.6',
     // Show the shortcuts bar in footer
     'shortcuts' => true,
 );
