8dfa0b136b38947bdd41c71c828ef262eb4de541
Andre Hansson <andre.hansson@etimo.se>
2023-02-03 09:13:47 +0100

    new regex matching

---
+++ src/assets/timewave.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+    // Version (is also used to prevent using of cached js files on update)
+    'version' => '2.6.4',
+    // Show the shortcuts bar in footer
+    'shortcuts' => true,
+);
