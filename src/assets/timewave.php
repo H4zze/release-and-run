c379ceb7a72a6d1980d5f9d74e2b3a21840a2386
Andre Hansson <andre.hansson@etimo.se>
2023-02-03 09:48:45 +0100

    final test

--- src/assets/timewave.php
+++ src/assets/timewave.php
@@ -2,7 +2,7 @@
 
 return array(
     // Version (is also used to prevent using of cached js files on update)
-    'version' => '2.7.0',
+    'version' => '2.7.1',
     // Show the shortcuts bar in footer
     'shortcuts' => true,
 );
